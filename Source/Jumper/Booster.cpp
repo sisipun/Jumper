55822bd341080aa206991885f7314c69fbd7e8a2
Sisipun <sisipun@gmail.com>
2021-12-16 22:03:53 +0300

    Booster collider overlap nethod

--- Source/Jumper/Booster.cpp
+++ Source/Jumper/Booster.cpp
@@ -4,17 +4,33 @@ ABooster::ABooster()
 {
 	PrimaryActorTick.bCanEverTick = true;
 
+	Collider = CreateDefaultSubobject<USphereComponent>(TEXT("Collider"));
 	Mesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Mesh"));
+
+	Collider->SetupAttachment(RootComponent);
+	Mesh->SetupAttachment(Collider);
 }
 
 void ABooster::BeginPlay()
 {
 	Super::BeginPlay();
-	
+
+	Collider->OnComponentBeginOverlap.AddDynamic(this, &ABooster::OnBeginOverlap);
 }
 
 void ABooster::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
+}
+
+void ABooster::OnBeginOverlap(
+	class UPrimitiveComponent* HitComponent,
+	class AActor* OtherActor,
+	class UPrimitiveComponent* OtherComponent,
+	int32 OtherBodyIndex,
+	bool bFromSweep,
+	const FHitResult& SweepResult
+)
+{
 }
 
--- Source/Jumper/Booster.h
+++ Source/Jumper/Booster.h
@@ -2,6 +2,7 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
+#include "Components/SphereComponent.h"
 #include "Components/StaticMeshComponent.h"
 #include "Booster.generated.h"
 
@@ -14,7 +15,20 @@ public:
 	ABooster();
 
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+		USphereComponent* Collider;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
 		UStaticMeshComponent* Mesh;
+
+	UFUNCTION()
+		void OnBeginOverlap(
+			class UPrimitiveComponent* HitComponent,
+			class AActor* OtherActor,
+			class UPrimitiveComponent* OtherComponent,
+			int32 OtherBodyIndex,
+			bool bFromSweep,
+			const FHitResult& SweepResult
+		);
 
 protected:
 	virtual void BeginPlay() override;
